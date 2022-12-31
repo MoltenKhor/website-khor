888563ba57a9c636f00eaeb72975facc9fa4d2b0
Marco Pontone <marcopontone@Marcos-MacBook-Pro-2.local>
2022-12-31 15:56:20 -0500

    new website 2023

--- functions.js
+++
@@ -1,9 +0,0 @@
-function switchVisibility(name){
-  var spacer = document.getElementById(name);
-  var allSpacers = document.body.getElementsByClassName('about');
-  for(var i = 0; i<allSpacers.length; i++){
-    allSpacers[i].style.visibility = "hidden";
-  }
-    spacer.style.visibility = "visible";
-
-}
